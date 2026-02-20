be744b61b5d314af622f196231a30b9bdd19bba8
elite421 <elitegames421@gmail.com>
2026-02-20 17:32:40 +0530

    page.js

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -2,6 +2,7 @@ import { readData } from '@/app/lib/dataManager';
 import CheckoutClient from './CheckoutClient';
 import { verifyUserSession } from '@/app/lib/auth';
 
+export const dynamic = 'force-dynamic';
 export const metadata = { title: 'Checkout | FoodiClub' };
 
 export default async function CheckoutPage() {
