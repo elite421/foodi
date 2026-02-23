d707309a19985602adca01b5d01263764332796f
elite421 <elitegames421@gmail.com>
2026-02-24 00:40:24 +0530

    Delete app directory

--- app/lib/prisma.js
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-const globalForPrisma = globalThis;
-
-const prisma = globalForPrisma.prisma2 ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma2 = prisma;
-
-export default prisma;
