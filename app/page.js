2b4f304addb29ded65ed35bca40f15001d651f89
elite421 <elitegames421@gmail.com>
2026-03-18 13:21:41 +0530

    Delete app directory

--- app/page.js
+++
@@ -1,9 +0,0 @@
-import HomeWrapper from './components/HomeWrapper';
-import { readData } from './lib/dataManager';
-
-export const dynamic = 'force-dynamic';
-
-export default async function Home() {
-  const data = await readData();
-  return <HomeWrapper data={data} />;
-}
